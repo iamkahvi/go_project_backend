bed61b45c4c60f1ae4e2fae216afa2b7bd3d6818
Kahvi Patel <iamkahvi@gmail.com>
2020-05-16 01:07:14 -0700

    updating DBS to DB object

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"example.com/gin_server/storage"
 	"github.com/gin-contrib/cors"
@@ -10,90 +9,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// ResponseBody : struct to bind the JSON response body
-type ResponseBody struct {
-	User string `json:"user"`
-}
-
-const m string = "pong"
-
-func pong(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-	fmt.Println(c.Request.Header)
-	fmt.Println(c.Request.Body)
-}
-
-func showUser(d *storage.DBS) gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		user := c.Params.ByName("name")
-		d.Users = append(d.Users, user)
-
-		d.Number++
-		// fmt.Println(c.Request.Proto)
-
-		value, ok := d.DB[user]
-		if ok {
-			c.JSON(200, gin.H{"user": user, "value": value, "something": d.PrintAll()})
-		} else {
-			c.JSON(200, gin.H{"user": user, "status": "no value"})
-		}
-	}
-}
-
-func changeUserValueString(d *storage.DBS) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		nameValue := c.Params.ByName("nameValue")
-		d.Number++
-
-		// fmt.Println(c.Request)
-		params := strings.Split(nameValue, "=")
-		user, value := params[0], params[1]
-
-		d.DB[user] = value
-		c.JSON(200, gin.H{"message": "saved"})
-	}
-}
-
-func changeUserValueQuery(d *storage.DBS) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		user := c.Query("user")
-		value := c.Query("value")
-		d.Number++
-
-		d.DB[user] = value
-		c.JSON(200, gin.H{"message": "saved"})
-	}
-}
-
-func post(d *storage.DBS) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		d.Number++
-
-		var rb ResponseBody
-		c.BindJSON(&rb)
-
-		d.Users = append(d.Users, rb.User)
-		fmt.Println(rb)
-
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.JSON(200, gin.H{"list": d.UserList()})
-	}
-}
-
-func fetchUserList(d *storage.DBS) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		d.Number++
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.JSON(200, gin.H{"list": d.UserList()})
-	}
-}
-
 func main() {
-	d := storage.DBS{Number: 0, DB: make(map[string]string)}
+	db := storage.DB{Number: 0}
+	db.InitDB()
 
 	r := gin.Default()
 
@@ -110,15 +28,95 @@ func main() {
 	})
 
 	r.GET("/ping", pong)
-	r.GET("/users", fetchUserList(&d))
+	r.GET("/users", fetchUserList(&db))
 
-	r.GET("/user/:name", showUser(&d))
+	// r.GET("/user/:name", showUser(&db))
 
-	r.GET("/change/:nameValue", changeUserValueString(&d))
+	// r.GET("/change/:nameValue", changeUserValueString(&db))
 
-	r.GET("/params", changeUserValueQuery(&d))
+	// r.GET("/params", changeUserValueQuery(&db))
 
-	r.POST("/post", post(&d))
+	r.POST("/post", post(&db))
 
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+const m string = "pong"
+
+func pong(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+	// fmt.Println(c.Request.Header)
+	// fmt.Println(c.Request.Body)
+}
+
+// func showUser(du *storage.DB) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+
+// 		user := c.Params.ByName("name")
+// 		d.Users = append(d.Users, user)
+
+// 		d.Number++
+// 		// fmt.Println(c.Request.Proto)
+
+// 		value, ok := d.DB[user]
+// 		if ok {
+// 			c.JSON(200, gin.H{"user": user, "value": value, "something": d.PrintAll()})
+// 		} else {
+// 			c.JSON(200, gin.H{"user": user, "status": "no value"})
+// 		}
+// 	}
+// }
+
+// func changeUserValueString(d *storage.DBS) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		nameValue := c.Params.ByName("nameValue")
+// 		d.Number++
+
+// 		// fmt.Println(c.Request)
+// 		params := strings.Split(nameValue, "=")
+// 		user, value := params[0], params[1]
+
+// 		d.DB[user] = value
+// 		c.JSON(200, gin.H{"message": "saved"})
+// 	}
+// }
+
+// func changeUserValueQuery(d *storage.DBS) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		user := c.Query("user")
+// 		value := c.Query("value")
+// 		d.Number++
+
+// 		d.DB[user] = value
+// 		c.JSON(200, gin.H{"message": "saved"})
+// 	}
+// }
+
+// ResponseBody : struct to bind the JSON response body
+type ResponseBody struct {
+	User string `json:"user"`
+}
+
+func post(d *storage.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var rb ResponseBody
+		c.BindJSON(&rb)
+
+		d.AddUser(rb.User)
+		fmt.Println(rb)
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.JSON(200, gin.H{"list": d.GetUsers()})
+	}
+}
+
+func fetchUserList(d *storage.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		d.Number++
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.JSON(200, gin.H{"list": d.GetUsers()})
+	}
 }
--- storage/main.go
+++ storage/main.go
@@ -1,31 +1,69 @@
 package storage
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 // Item : a struct to store a key and value combo
 type Item struct {
-	key   string
-	value string
+	key string
 }
 
-// DBS : The database struct to store stuff from the server
-type DBS struct {
-	Users  []string
-	Items  []Item
+// User : The user object for storing a user's info
+type User struct {
+	gorm.Model
+	Name string
+}
+
+// DB : The database struct to store stuff from the server
+type DB struct {
+	User   User
+	Item   Item
 	Number int
-	DB     map[string]string
+	db     *gorm.DB
+}
+
+// InitDB : This gets the sql thing idk
+func (d *DB) InitDB() {
+	db, err := gorm.Open("mysql", "root@/users?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect to the database :(")
+	}
+	// defer db.Close()
+
+	db.AutoMigrate(&User{})
+
+	d.db = db
+}
+
+// AddUser : Method to add user to db
+func (d *DB) AddUser(name string) int {
+	d.db.Create(&User{Name: name})
+	var user User
+	d.db.First(&user, "name = ?", "Kahvi")
+
+	fmt.Println("\n" + user.Name + "\n" + "Fuck yeah")
+
+	return 1
 }
 
 // PrintAll : Method to output the number field
-func (d *DBS) PrintAll() map[string]interface{} {
+func (d *DB) PrintAll() map[string]interface{} {
 	return gin.H{
-		"number": d.Number,
+		"number": 10,
 	}
 }
 
-// UserList : Method to ouput item list
-func (d *DBS) UserList() map[string]interface{} {
+// GetUsers : Method to get list of users
+func (d *DB) GetUsers() map[string]interface{} {
+	var users []User
+	d.db.Find(&users)
 	return gin.H{
-		"users": d.Users,
+		"users": users,
 	}
 }
